18e5ad94384ef8214b7f9643239b3b52daa66365
unknown <namlong9012@gmail.com>
2020-10-15 21:01:38 +0700

    change port

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const sessionMiddleware = require('./middlewares/session.middleware')
 const errorHandling = require('./middlewares/error.handling')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 mongoose.connect(process.env.MONGO_URL)
 
