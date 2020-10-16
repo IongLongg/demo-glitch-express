95b6ffda79449d50950b8d787f04687f894b461a
unknown <namlong9012@gmail.com>
2020-10-16 09:17:47 +0700

    auth signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,6 @@
+const cloudinary = require('cloudinary').v2
+const bcrypt = require('bcrypt')
+
 const Session = require('../models/session.model')
 const User = require('../models/user.model')
 
@@ -19,4 +22,21 @@ module.exports.logout = async (req,res) => {
     res.clearCookie('sessionId')
     await Session.findByIdAndDelete(req.signedCookies.sessionId)
     res.redirect('/login')
+}
+
+module.exports.signup =  (req, res) => {
+    res.render('users/create')
+}
+module.exports.postSignup = async (req, res) => {
+    await cloudinary.uploader.upload(`./${req.file.path}`, (error, result) => {
+        if(!error){
+            req.body.avatar = result.url
+        } else {
+            console.log(error)
+        }
+    })
+    let hashPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashPassword
+    await User.create(req.body)
+    res.redirect('/auth/login')
 }
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const multer = require('multer')
 
 const controller = require('../controllers/auth.controller')
 const validator = require('../validators/auth.validator')
 
 const router = express.Router()
+const upload = multer({dest : './public/uploads/'})
 
 router.get('/login', controller.login)
 router.post('/login', validator.postLogin ,controller.postLogin)
 
 router.get('/logout', controller.logout)
+
+router.get('/signup', controller.signup)
+router.post('/signup', upload.single('avatar') ,controller.postSignup)
 
 module.exports = router
